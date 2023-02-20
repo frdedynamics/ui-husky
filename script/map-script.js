588b9397cafa894db16abd25329d27f1330af881
William <590413@stud.hvl.no>
2023-02-20 16:25:53 +0100

    Scaling implemented

---
+++ script/map-script.js
@@ -0,0 +1,27 @@
+var popup = L.popup();
+
+// Runs on buttonpress
+function buttonPress() {
+    var polygon = L.polygon([
+    [61.45886, 5.88614],
+    [61.45897, 5.88665],
+    [61.45884, 5.88676],
+    [61.45872, 5.88627]
+]).addTo(map);
+}
+
+// Runs when map is clicked
+function onMapClick(e) {
+    popup
+    .setLatLng(e.latlng)
+    .setContent("Dette er: " + e.latlng.toString())
+    .openOn(map);
+} 
+
+    var map = L.map('map').setView([61.45874, 5.88740], 18);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
