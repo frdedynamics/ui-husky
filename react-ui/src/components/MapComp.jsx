ab90568388353ff0b00c50ad4a5d77c71090dfff
William <590413@stud.hvl.no>
2023-03-16 15:11:58 +0100

    Zoom level on map
    Fixed zoom level on map to go past 19 zoom level and still show map

--- react-ui/src/components/MapComp.jsx
+++ react-ui/src/components/MapComp.jsx
@@ -1,25 +1,55 @@
 import React, { Component } from 'react'
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import { MapContainer, TileLayer, Marker, Popup, useMapEvents } from 'react-leaflet'
 import "leaflet/dist/leaflet.css"
 import { Icon } from 'leaflet'
+import { useMap } from 'react-leaflet/hooks'
 
-const cicon = new Icon({
-  iconUrl: "https://cdn-icons-png.flaticon.com/512/2776/2776000.png", // kan ref fil her også
-  iconSize: [38,38]
-})
+
+
 class MapComp extends Component {
-  state = {
-  }
+  state = { connected: false, ros: null, huskypos: [61.45874, 5.88743] } 
+  
+  componentDidMount() {
+    const ros = new window.ROSLIB.Ros();
+    ros.on("connection", () => {
+      console.log("Connection established! MapComp")
+      this.setState({ connected: true, ros: ros })
+    });
+    
+        ros.on("close", () => {
+          console.log("Connection is closed! MapComp");
+          this.setState({ connected: false, ros: null });
+          // Retry every second
+          setTimeout(() => {
+            try {
+              ros.connect("ws://" + process.env.REACT_APP_IP_ROS + ":" + process.env.REACT_APP_PORT_ROS + "");
+            } catch (error) {
+              console.log("connection problem MapComp");
+            }
+          }, Number(process.env.REACT_APP_REFRESH_TIMER))
+        });
+    
+        try {
+          ros.connect("ws://" + process.env.REACT_APP_IP_ROS + ":" + process.env.REACT_APP_PORT_ROS + "");
+        } catch (error) {
+          console.log("connection problem MapComp");
+        }
+      }
+
   render() {
+    const cicon = new Icon({
+      iconUrl: "https://cdn-icons-png.flaticon.com/512/2776/2776000.png", // kan ref fil her også
+      iconSize: [38,38]
+    })
     return (
-      <MapContainer center={[61.45874, 5.88743]} zoom={18} scrollWheelZoom={false}>
-        <TileLayer
+      <MapContainer center={[61.45874, 5.88743]} zoom={18} scrollWheelZoom={false} maxZoom={25}>
+        <TileLayer maxNativeZoom={19} maxZoom={25}
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={[61.45874, 5.88743]} icon={cicon}>
+        <Marker position={this.state.huskypos} icon={cicon}>
           <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
+            Husky <br /> Mobile robot
           </Popup>
         </Marker>
       </MapContainer>
